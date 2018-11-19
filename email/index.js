6a69f9155e49382a30c70d4951b3291d1f152c20
tuhina2020 <tuhinatwyla@gmail.com>
2018-11-19 12:20:20 +0530

    email file content

--- email/index.js
+++ email/index.js
@@ -6,13 +6,22 @@ const async = require('async');
 const fs = require('fs');
 const path = require('path');
 
-const sendmail = (directory, files, API_KEY) => {
+function base64_encode(file) {
+    // read binary data
+    var bitmap = fs.readFileSync(file);
+    // convert binary data to base64 encoded string
+    return new Buffer(bitmap).toString('base64');
+}
+
+const sendmail = (directory, files, API_KEY, tag) => {
 	const transport = nodemailer.createTransport(mandrillTransport({
       auth: { apiKey: API_KEY }
     }));
+  console.log('files are this : ', files);
     const attachments = _.map(files, file => {
+      const filepath = path.join(directory, file);
     	return {
-    	  'content': 'ZXhhbXBsZSBmaWxl',
+    	  'content': base64_encode(filepath),
           'name': file,
           'type': 'text/plain'
     	}
@@ -35,10 +44,15 @@ const sendmail = (directory, files, API_KEY) => {
         'email': 'krishna.kulkarni@elanic.in',
         'name': 'Krishna',
         'type': 'to'
-      }],
+      }
+      ],
       important: false,
       merge: true,
       merge_language: 'handlebars',
+      global_merge_vars: [{
+        name: 'tag',
+        content: tag
+      }]
     };
   var mandrill_client = new mandrill.Mandrill(API_KEY);
 	return transport.sendMail({
@@ -68,4 +82,4 @@ const API_KEY = process.argv[4]
 const timestamp = new Date().toISOString().split('T')[0];
 const r = tag + '_' + timestamp;
 const files = fs.readdirSync(p);
-sendmail(p, files, API_KEY)
+sendmail(p, files, API_KEY, tag)
